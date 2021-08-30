9f257b3df31787de49c488224f659a8417b49fef
Abhilash D G <63736639+Abhilashdg@users.noreply.github.com>
2021-08-30 18:01:20 +0530

    Add files via upload

---
+++ AUG 13 Assign Numbers Q1-ABHILASH D G.py
@@ -0,0 +1,8 @@
+'''
+1.Write a program that reads a positive integer, n, from the user and then displays the sum of all of the integers from 1 to n. 
+The sum of the first n positive integers can be computed using the formula: sum = (n)(n + 1) / 2 
+'''
+
+n = int(input('Enter a number >'))     #input from the user
+sum1 = (n)*(nr + 1) / 2       #sum of number form 1 to given number 
+print(int(sum1))
