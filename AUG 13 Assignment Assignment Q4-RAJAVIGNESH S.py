74baae02a0b928fb888a25a2f30333d947e1ef55
rajavignesh64 <69569314+rajavignesh64@users.noreply.github.com>
2021-08-30 18:17:07 +0530

    Add files via upload

---
+++ AUG 13 Assignment Assignment Q4-RAJAVIGNESH S.py
@@ -0,0 +1,9 @@
+'''
+6.ACCEPT A NUMBER FROM THE USER AND PRINT IF IT IS ‘EVEN’ OR ‘ODD’
+'''
+n= int(input('Enter a number >'))     #enter a number to find it is even or not
+
+if n % 2 == 0:                         #if n % 2 is equal to 0 then it will print even
+    print(n,"is even number")          
+else:
+    print(n,"is odd number")  #if not zero then it is odd this statement will print
