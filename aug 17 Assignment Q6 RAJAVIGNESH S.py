ee990dfb5b6b2ec88697c9ad1d694b87ec656ae1
rajavignesh64 <69569314+rajavignesh64@users.noreply.github.com>
2021-08-30 21:52:02 +0530

    Add files via upload

---
+++ aug 17 Assignment Q6 RAJAVIGNESH S.py
@@ -0,0 +1,32 @@
+"""EX-2
+>>> msg = 'Hello world, Python people!' 
+>>> print(msg[2:12]) 
+>>> print(msg[2:12:1]) 
+>>> print(msg[12:2]) 		
+>>> print(msg[12: 2:-1]) 
+>>> print(msg[2:12:2]) 
+>>> print(msg[5:18:3]) 
+>>> print(msg[25: 8:1]) 	 
+>>> print(msg[22:6:-1]) 
+>>> print(msg[-2:8:-2]) 
+>>> print(msg[:12]) 		 
+>>> print(msg[:]) 		 
+>>> print(msg[-3:-20]) 		 
+>>> print(msg[-3:2]) 		 
+>>> print(msg[::-1])"""
+
+msg = 'Hello world, Python people!' 
+print(msg[2:12])
+print(msg[2:12:1])
+print(msg[12:2])
+print(msg[12: 2:-1]) 
+print(msg[2:12:2]) 
+print(msg[5:18:3]) 
+print(msg[25: 8:1]) 	 
+print(msg[22:6:-1]) 
+print(msg[-2:8:-2]) 
+print(msg[:12]) 		 
+print(msg[:]) 		 
+print(msg[-3:-20])
+print(msg[-3:2])
+print(msg[::-1])
