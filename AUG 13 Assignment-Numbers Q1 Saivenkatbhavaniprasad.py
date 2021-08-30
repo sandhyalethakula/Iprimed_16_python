b060c4ed0f755c2e4fd84e114553c9ba87f6ec23
Michael Devine <developer.michaeldevine@gmail.com>
2021-08-30 16:24:12 +0530

    Update AUG 13 Assignment-Numbers Q1 Saivenkatbhavaniprasad.py

--- AUG 13 Assignment-Numbers Q1 Saivenkatbhavaniprasad.py
+++ AUG 13 Assignment-Numbers Q1 Saivenkatbhavaniprasad.py
@@ -1,5 +1,6 @@
 '''
-1.Write a program that reads a positive integer, n, from the user and then displays the sum of all of the integers from 1 to n. The sum of the first n positive integers can be computed using the formula: sum = (n)(n + 1) / 2 
+1.Write a program that reads a positive integer, n, from the user and then displays the sum of all of the integers from 1 to n. 
+The sum of the first n positive integers can be computed using the formula: sum = (n)(n + 1) / 2 
 
 '''
 
