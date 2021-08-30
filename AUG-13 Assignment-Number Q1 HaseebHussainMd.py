efe6aa72ae1962c00a074cd848a00ae7efa48387
HaseebHussainMd <89777812+HaseebHussainMd@users.noreply.github.com>
2021-08-30 19:30:25 +0530

    Add files via upload

---
+++ AUG-13 Assignment-Number Q1 HaseebHussainMd.py
@@ -0,0 +1,10 @@
+'''
+1.Write a program that reads a positive integer, n, from the user
+and then displays the sum of all of the integers from 1 to n.
+The sum of the first n positive integers can be computed using the formula: sum = (n)(n + 1) / 2
+'''
+
+#sum of positive integers
+n=int(input('Enter a number :'))
+h=(n)*(n+1)/2
+print('The sum of positive integers from 1 to n is:',h)
---
+++ AUG-13 Assignment-Number Q3 HaseebHussainMd.py
@@ -0,0 +1,9 @@
+
+'''
+3.Write a program that begins by reading a temperature from the user in degrees Celsius.
+Then your program should display the equivalent temperature in degrees Fahrenheit. (fahrenheit = (celsius * 9/5) + 32)
+'''
+#celcius to farhenheit
+n=float(input('Enter degree celcius :'))
+f=(n*9/5)+32
+print('farhenheit value =:',f)
