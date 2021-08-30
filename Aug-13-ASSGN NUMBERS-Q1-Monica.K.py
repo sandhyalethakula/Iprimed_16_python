dde7028dbd99c9d0d9d45c57fbe1d478a76e338a
monica-23 <89778209+monica-23@users.noreply.github.com>
2021-08-30 18:51:50 +0530

    Add files via upload

---
+++ Aug-13-ASSGN NUMBERS-Q1-Monica.K.py
@@ -0,0 +1,4 @@
+'''Write a program that reads a positive integer, n, from the user and then displays the sum of all of the integers from 1 to n. The sum of the first n
+positive integers can be computed using the formula: sum = (n)(n + 1) / 2 '''
+n=int(input("Enter a number : "))
+print("The sum ofof all the natural numbers till ",n,'is',n*(n+1)/2)
---
+++ Aug-13-ASSGN NUMBERS-Q2-Monica.K.py
@@ -0,0 +1,14 @@
+# A retailer sells two products: Apples and Oranges. Each apple weighs 75 grams.
+# Each orange weighs 112 grams. Write a program that reads the number of apples
+# and the number of oranges in an order from the user. Then your program
+# should compute and display the total weight of the order.
+
+
+a=75
+b=112
+a1=int(input("Enter the number of apples that you bought : "))
+b1=int(input("Enter the number of oranges that you bought : "))
+w=a*a1+b*b1
+print ("The total weight of",a1,"apples is : ",a*a1,"grams ")
+print ("The total weight of",b1,"oranges is : ",b*b1,"grams ")
+print ("The total weight of fruits is : ",w,"grams ie;",w/1000,"Kg")
