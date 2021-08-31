cff66b4f8649ae5d9ab9297c436fa2aa75b29c60
Aprameyan-V <89632505+Aprameyan-V@users.noreply.github.com>
2021-08-31 07:39:59 +0530

    Add files via upload

---
+++ Aug-16-ASSGN FlowControl-Q1-AprameyanV.py
@@ -0,0 +1,25 @@
+"""ACCEPT A NUMBER BETWEEN 5 AND 30 FROM THE USER.
+   a.IF THE NUMBER IS LESS THAN 10, MULTIPLY BY 5 AND PRINT THE RESULT
+   b.IF THE NUMBER IS > 25, SUBTRACT 5 AND PRINT THE RESULT
+   c.IF THE NUMBER IS >=10 AND <=15, ADD 5 AND PRINT THE RESULT
+   d.IF THE NUMBER IS >20, DIVIDE BY 5 AND PRINT THE RESULT
+   e.IF THE NUMBER DOES NOT SATISFY THESE CONDITIONS, PRINT THE NUMBER"""
+
+
+
+
+n=int(input("enter a number between 5 and 30"))
+if n<10:
+    res=n*5
+    print("Result",res)
+elif n>25:
+    res=n-5
+    print("Result",res)
+elif n>=10 and n<=15:
+    res=n+5
+    print("Result",res)
+elif n>=20:
+    res=n//5
+    print("result",res)
+else:
+    print("Result",n)
---
+++ Aug-16-ASSGN FlowControl-Q4-AprameyanV.py
@@ -0,0 +1,17 @@
+""" Ask the user to enter username
+       if the user name is wrong then ask user to enter the username again
+            if user is correct then ask the user to enter the password
+                 if password is wrong repeat the process
+                      if both are correct then greet the user"""
+
+
+
+
+
+while True:
+    name=input("enter user name")
+    if name=="admin":
+        paass=input("enter password")
+        if paass=="admin123":
+          print("HELLO",name,paass)
+          break
