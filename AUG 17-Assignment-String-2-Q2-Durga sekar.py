401705b67258ecf342d0ce7a974b7e3e50ad9558
Durga-sekar <89777370+Durga-sekar@users.noreply.github.com>
2021-08-31 14:20:42 +0530

    Add files via upload

---
+++ AUG 17-Assignment-String-2-Q2-Durga sekar.py
@@ -0,0 +1,37 @@
+'''
+print string output using string slicing
+print(msg[2:12]) 
+print(msg[2:12:1]) 
+print(msg[12:2]) 		
+print(msg[12: 2:-1]) 
+print(msg[2:12:2]) 
+print(msg[5:18:3])                                  
+print(msg[25: 8:1]) 	 
+print(msg[22:6:-1]) 
+print(msg[-2:8:-2]) 
+print(msg[:12]) 		 
+print(msg[:]) 		 
+print(msg[-3:-20]) 		 
+print(msg[-3:2]) 		 
+print(msg[::-1]) 
+
+'''
+print()
+print()
+print()
+
+msg = 'Hello world, Python people!'                 #prints the input sentence into different output using string slicing
+print(msg[2:12]) 
+print(msg[2:12:1]) 
+print(msg[12:2]) 		
+print(msg[12: 2:-1]) 
+print(msg[2:12:2]) 
+print(msg[5:18:3])                                  
+print(msg[25: 8:1]) 	 
+print(msg[22:6:-1]) 
+print(msg[-2:8:-2]) 
+print(msg[:12]) 		 
+print(msg[:]) 		 
+print(msg[-3:-20]) 		 
+print(msg[-3:2]) 		 
+print(msg[::-1]) 
---
+++ AUG 17-Assignment-String-Q1-Durga sekar.py
@@ -0,0 +1,23 @@
+'''
+1.Ask the user to input And print the address neatly. 
+	A. House number ? 
+	B. Street info ? 
+	C. Colony info ? 
+	D. Landmark ? 
+	E. City 
+	F. Country 
+	G. Pincode 
+'''
+
+house_num=input("Enter your house number >")        #enter house number
+street_name=input("Enter your street info >")       #enter street info
+colony_name=input("Enter your colony info >")       #enter colony info
+landmark=input("Enter your landmark >")             #enter landmark
+city=input("Enter your city name >")                #enter city name
+country=input("Enter country name >")               #enter country name
+pincode=input("Enter your city pincode >")          #enter city pincode
+
+print("House number:- "+house_num, "Street info:- "+ street_name, "Colony info:- "+colony_name , "Landmar:- "+landmark,  "City:- "+city ,"pincode:- "+pincode,  "Country:- "+country, sep="\n")
+
+
+
