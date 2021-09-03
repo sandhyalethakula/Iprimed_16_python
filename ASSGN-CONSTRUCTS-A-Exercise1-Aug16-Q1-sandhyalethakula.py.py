5b046218048a66f3a92252f1ad7701970b057ea8
sandhyalethakula <89776579+sandhyalethakula@users.noreply.github.com>
2021-09-03 06:21:19 +0530

    Add files via upload

---
+++ ASSGN-CONSTRUCTS-A-Exercise1-Aug16-Q1-sandhyalethakula.py.py
@@ -0,0 +1,17 @@
+'''RUN THE PROGRAM..
+What is the output that printed on the screen
+'''
+
+
+x = 4
+while x >= 1:
+    if x % 4 == 0 :
+        print ("party")
+    elif x -2 < 0:
+        print("cake")
+    elif x / 3 == 0:
+        print("greeting")
+    else:
+        print ("birthday")
+    x = x-1
+              
