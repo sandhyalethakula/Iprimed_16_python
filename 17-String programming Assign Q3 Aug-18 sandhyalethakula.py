d838149b8deb37be8a1e12393ee2470cd5ab47f5
sandhyalethakula <89776579+sandhyalethakula@users.noreply.github.com>
2021-09-03 06:58:33 +0530

    Add files via upload

---
+++ 17-String programming Assign Q3 Aug-18 sandhyalethakula.py
@@ -0,0 +1,16 @@
+'''write a program that asks the user to enter a string s (multiple sentences) and then converts s to lowercase, removes all the periods and commas from s, and prints the resulting string.'''
+
+
+str=input("enter a string:")
+word=''
+for i in str:
+	if i.isalpha():
+		word+=i.lower()             #print each word in lower case
+	else:
+		word+=""
+print(word)
+
+
+
+
+
